248f7b83b5cb6d958acf98eb0533c93511facfa6
Matt Cowell <matthew.cowell@gmail.com>
2022-03-11 18:48:20 -0600

    touch_parser: refactor and add improvements to printing

--- t5uid1/480x272/DGUSM/touch_parser.py
+++ t5uid1/480x272/DGUSM/touch_parser.py
@@ -1,31 +1,46 @@
-# from struct import *
-
-# class TouchControl:
-#     decoder = Struct('>H HHHH HHH')
-
-#     def __init__(buf):
-#         (self.pic, self.Xstart, self.Ystart, self.Xend, self.Yend, self.pic_next, self.pic_touched, self.type, self.subtype) = decoder.unpack(buf)
-
-from logging.config import valid_ident
 from mmap import *
 from pathlib import Path
 from ctypes import *
-from turtle import textinput
+
+class VP:
+    def __init__(self, word_addr=None, size=None) -> None:
+        self.addr = word_addr * 2 if word_addr is not None else None
+        self.size = size
+        self.bit = None
+
+    def set_bit_mode(self, bit):
+        assert self.bit is None and bit in range(16)
+        self.bit = bit
+        self.size = 1
+        if bit < 8:
+            self.addr_bytes += 1
+        else:
+            bit -= 8
+
+    def __str__(self) -> str:
+        if self.bit is None:
+            return 'VP {:04x} +{:x}'.format(self.addr, self.size)
+        else:
+            return 'VP {:04x} b{}'.format(self.addr, self.bit)
 
 class Coord(BigEndianStructure):
     _pack_ = 1
     _fields_ = [("x", c_uint16),
                 ("y", c_uint16)]
 
+    def __sub__(self, other):
+        return Coord(self.x - other.x, self.y - other.y)
+
     def __str__(self) -> str:
-        return '({}, {})'.format(self.x, self.y)
+        return '({:3},{:3})'.format(self.x, self.y)
 
 class Area(BigEndianStructure):
     _pack_ = 1
     _fields_ = [("start", Coord),
                 ("end", Coord)]
+
     def __str__(self) -> str:
-        return 'start:{} size:{}'.format(self.start, self.end - self.start)
+        return '@{} +{}'.format(self.start, self.end - self.start)
 
 class TouchArea(BigEndianStructure):
     _pack_ = 1
@@ -50,6 +65,9 @@ class TouchArea(BigEndianStructure):
     def __init__(self, buf, off) -> None:
         pass
 
+    def __str__(self) -> str:
+        return 'P{:<3} {}'.format(self.pic, self.area)
+
 class Key:
     special_keycodes = {
         0xf0: 'cancel',
@@ -70,7 +88,7 @@ class Key:
         elif chr(self.code).isprintable():
             return chr(self.code)
         else:
-            return':0x{:02x}'.format(self.code)
+            return ':0x{:02x}'.format(self.code)
 
 class KeypadKey(TouchArea):
     type_codes = (0x00,)
@@ -80,10 +98,10 @@ class KeypadKey(TouchArea):
         self.key = Key(self.subtype)
 
     def __str__(self) -> str:
-        return 'keypad:{}'.format(self.key)
+        return '{} keypad:{}'.format(super().__str__(), self.key)
 
 class KeyboardKey(TouchArea):
-    type_codes = range(1, 0x7f)
+    type_codes = range(1, 0x80)
 
     def __init__(self, buf, off) -> None:
         super().__init__(buf, off)
@@ -91,15 +109,19 @@ class KeyboardKey(TouchArea):
         self.lower = Key(self.subtype)
 
     def __str__(self) -> str:
-        return 'keyboard:⇩{}⇧{}'.format(self.lower, self.upper)
+        return '{} keyboard:⇩{}⇧{}'.format(super().__str__(), self.lower, self.upper)
 
 class TouchControl(TouchArea):
     _pack_ = 1
     _fields_ = [("_continue0", c_uint8),
-                ("vp", c_uint16)]
+                ("vp_words", c_uint16)]
 
     type_codes = (0xfd, 0xfe)
     subtypes = {}
+
+    def __init__(self, buf, off) -> None:
+        super().__init__(buf, off)
+        self.vp = VP(self.vp_words)
 
     def __init_subclass__(cls) -> None:
         cls.subtypes[cls.subtype_code] = cls
@@ -115,7 +137,7 @@ class TouchControl(TouchArea):
         return self.subtypes[self.subtype]
 
     def __str__(self) -> str:
-        return 'control:{}'.format(self.__class__.__name__)
+        return '{} control:{} {}'.format(super().__str__(), self.__class__.__name__, self.vp)
 
 class NumericInput(TouchControl):
     subtype_code = 0x00
@@ -140,6 +162,22 @@ class NumericInput(TouchControl):
                 ("limit_max", c_int32),
                 ("_reserved", c_uint8 * 6)]
 
+    def __init__(self, buf, off) -> None:
+        super().__init__(buf, off)
+        if 0 == self.vp_format:
+            self.vp.size = 2
+        elif 1 == self.vp_format:
+            self.vp.size = 4
+        elif 2 == self.vp_format:
+            self.vp.size = 1
+        elif 3 == self.vp_format:
+            self.vp.addr += 1
+            self.vp.size = 1
+        elif 4 == self.vp_format:
+            raise ValueError('8 byte integers not supported on DGUSM')
+        else:
+            raise ValueError(self.vp_format)
+
 class SliderInput(TouchControl):
     subtype_code = 0x03
     _pack_ = 1
@@ -149,6 +187,18 @@ class SliderInput(TouchControl):
                 ("min", c_uint16),
                 ("max", c_uint16)]
 
+    def __init__(self, buf, off) -> None:
+        super().__init__(buf, off)
+        if 0 == self.vp_format:
+            self.vp.size = 2
+        elif 1 == self.vp_format:
+            self.vp.size = 1
+        elif 2 == self.vp_format:
+            self.vp.addr += 1
+            self.vp.size = 1
+        else:
+            raise ValueError(self.vp_format)
+
 class ButtonInput(TouchControl):
     subtype_code = 0x05
     _pack_ = 1
@@ -156,6 +206,21 @@ class ButtonInput(TouchControl):
                 ("vp_format", c_uint8, 4),
                 ("keycode", c_uint16),
                 ("_reserved", c_uint8 * 10)]
+
+    def __init__(self, buf, off) -> None:
+        super().__init__(buf, off)
+        if self.bit_mode:
+            self.vp.set_bit_mode(self.vp_format)
+        else:
+            if 0 == self.vp_format:
+                self.vp.size = 2
+            elif 1 == self.vp_format:
+                self.vp.size = 1
+            elif 2 == self.vp_format:
+                self.vp.addr += 1
+                self.vp.size = 1
+            else:
+                raise ValueError(self.vp_format)
 
 class TextInput(TouchControl):
     subtype_code = 0x06
@@ -179,8 +244,17 @@ class TextInput(TouchControl):
                 ("unmasked", c_uint8),
                 ("_reserved", c_uint8 * 10)]
 
+    def __init__(self, buf, off) -> None:
+        super().__init__(buf, off)
+        #FIXME: is this +1 correct?
+        self.vp.size = (self.vp_len_words + 1) * 2
+        if self.prefix_len:
+            self.vp.addr -= 2
+            self.vp.size += 2
+
 class Parser:
-    def make_class(self, mm, off) -> object:
+    @staticmethod
+    def make_class(mm, off) -> object:
         touch = TouchArea(mm, off)
         while True:
             t = touch.get_subclass()
@@ -197,6 +271,7 @@ class Parser:
         self.controls = []
 
         with open(filename, 'r+b') as f:
+            # cannot be read-only if we want to use the buffer directly
             #mm = mmap(f.fileno(), 0, access=ACCESS_READ)
             mm = mmap(f.fileno(), 0)
             # print(filename, 'len', len(mm))
@@ -208,9 +283,10 @@ class Parser:
                 # print('off: {:04x}'.format(off))
                 t = self.make_class(mm, off)
                 off += sizeof(t)
-                print(t)
                 self.controls.append(t)
 
 
 if __name__ == "__main__":
     p = Parser('.')
+    for c in p.controls:
+        print(c)
